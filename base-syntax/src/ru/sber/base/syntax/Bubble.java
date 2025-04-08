89623d0aa16e7470515d819626f7cdec127d6f16
Dm1-Vol <volynkinmitya@yandex.com>
2025-04-08 07:12:10 +0300

    Задание 2

--- base-syntax/src/ru/sber/base/syntax/Bubble.java
+++ base-syntax/src/ru/sber/base/syntax/Bubble.java
@@ -1,10 +1,33 @@
 package ru.sber.base.syntax;
 
-import static java.lang.Math.floor;
-import static java.lang.Math.random;
+import static java.lang.Math.*;
 
 public class Bubble {
     public static void main(String[] args){
-        
+        Integer[] a = new Integer[30];
+        for (int i = 0; i <= 29; i++) {
+            a[i] = (int) (floor(1000*random()));
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        int t = 0;
+        boolean stop = true;
+        for(int i = 1; i < 30; i++){
+            stop = true;
+            for(int j = 0; j < (30 - i); j++){
+                if(a[j] > a[j+1]){
+                    t = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = t;
+                    stop = false;
+                }
+            }
+            if(stop){
+                break;
+            }
+        }
+        for (int i = 0; i <= 29; i++) {
+            System.out.print(a[i] + " ");
+        }
     }
 }
